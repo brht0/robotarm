19b143111f9edd574a668bf1f58b8d1e6eef2845
Karitsaa <karipeso@gmail.com>
2022-10-27 13:38:11 +0300

    moveSteps works with negative angles.

--- doubleservotest/doubleservotest.ino
+++ doubleservotest/doubleservotest.ino
@@ -3,38 +3,73 @@
 Servo myservo5;  // (pin5) create servo object to control a servo
 Servo myservo9;  // (pin9)
 
+int pos5;
+int pos9;
+
+
+/*
 void drop5() {       // servo position 0 (min)
+  int oldPos5 = myservo5.read();                                              
+  for (pos5 = oldPos5; pos5 > 0; pos5 -= 1){                            
+    myservo5.write(pos5);
+    delay(15);
+  }
+}*/
+
+void drop5() {
   myservo5.write(0);
 }
+
 void drop9() {       
   myservo9.write(0);
 }
 
-void grab5() {         // servo position 180 (max)
+
+void grab5() {  
   myservo5.write(180);
 }
-void grab9() {         
+
+void grab9() {  
   myservo9.write(180);
 }
+
 
 void grasp5(int angle) {  // sets the servo angle 0 - 180 (90 in the middle)
   myservo5.write(angle);
 }
+
 void grasp9(int angle) {  
   myservo9.write(angle);
 }
 
-int pos9 = myservo9.read();
 
-//toimii ekan kerran, ei liikuta toisella
 void moveSteps5(int degreesToMove) {
-  int pos5 = myservo5.read();                                              //int oldPos5 = pos5;
-  pos5 = 0;
-  for (pos5; pos5 <= degreesToMove; pos5 += 1){                            //for (oldPos5 = pos5; oldPos5 <= (oldPos5 + degreesToMove); pos5 += 1){
+  int oldPos5 = myservo5.read();                                              
+  if (degreesToMove > 0) {
+    for (pos5 = oldPos5; pos5 < (oldPos5 + degreesToMove); pos5 += 1){
     myservo5.write(pos5);
     delay(15);
-  }
+  }} else {
+  for (pos5 = oldPos5; pos5 > (oldPos5 + degreesToMove); pos5 -= 1){
+    myservo5.write(pos5);
+    delay(15);  
+  }}
 }
+
+void moveSteps9(int degreesToMove) {
+  int oldPos9 = myservo9.read();                                              
+  if (degreesToMove > 0) {
+    for (pos9 = oldPos9; pos9 < (oldPos9 + degreesToMove); pos9 += 1){
+    myservo9.write(pos9);
+    delay(15);
+  }} else {
+  for (pos9 = oldPos9; pos9 > (oldPos9 + degreesToMove); pos9 -= 1){
+    myservo9.write(pos9);
+    delay(15);  
+  }}
+}
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -43,12 +78,13 @@ void setup() {
 }
 
 void loop() {
-  moveSteps5(90);
-  delay(10000);
-  moveSteps5(90);
-  delay(5000);
+  moveSteps5(60);
+  delay(1500);
+  moveSteps5(-30);
+  delay(1500);
   drop5();
-  delay(10000);
+  delay(1500);
+ 
 
 
 
